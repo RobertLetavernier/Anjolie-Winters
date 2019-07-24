3bbfa6fd11e55f1de26b8f80ae0bc97156071d17
RobertLetavernier <robert.letavernier@gmail.com>
2019-07-25 01:06:07 +0200

    tweeks

--- flatplan.php
+++ flatplan.php
@@ -9,6 +9,14 @@ function getCsv($input) {
 	return str_getcsv ( $input, ',', '"', '\\');
 }
 
+function pictify($str) {
+	$ret = "<span>{$str}</span>";
+	if (is_file("picto/{$str}")) {
+		$ret = "<img src=\"picto/{$str}\" />";
+	}
+	return $ret;
+}
+
     $csv = array_map('getCsv', file($file));
     array_walk($csv, function(&$a) use ($csv) {
       $a = array_combine($csv[0], $a);
@@ -20,6 +28,7 @@ function getCsv($input) {
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="tpt.css">
+	<link href="https://fonts.googleapis.com/css?family=Chewy|Eagle+Lake|Indie+Flower|Knewave|Permanent+Marker|Roboto|Shojumaru&display=swap" rel="stylesheet"> 
 </head>
 <body>
 <?php
@@ -39,27 +48,27 @@ function getCsv($input) {
  * Ability box
  */
  foreach ($csv as $i => $card) {
-	if ($i < 15 OR $i > 45) continue;
+	// if ($i < 15 OR $i > 45) continue;
     echo "<div class=\"cardFrame\">                            ";
 	echo '<!-- ' . print_r($card, 1) . ' -->';
     echo "<div class=\"cardBorder\">                            ";
     echo "<div class=\"cardBackground color-{$card['color']}\" style=\"background-image: url('img/{$card['id']}.jpg');\">                            ";
     echo "	<div class=\"container title\">                  ";
-    echo !$card['color'] ? '' : "		<div class=\"inner picto\"><img src=\"picto/{$card['color']}\" /></div>";
-    echo !$card['suit']  ? '' : "		<div class=\"inner picto\"><img src=\"picto/{$card['suit']}\"  /></div>";
-    // echo "		<div class=\"inner head\">{$card['Bang name']}</div> ";
+    echo !strlen($card['s1']) ? '' : "	<div class=\"inner picto\">" . pictify($card['s1']) . "</div>";
+    echo !strlen($card['s2']) ? '' : "	<div class=\"inner picto\">" . pictify($card['s2']) . "</div>";
     echo "	</div>                                           ";
     echo !strlen($card['p1']) ? '' : "	<div class=\"container left-bar\">               ";
-    echo !strlen($card['p1']) ? '' : "		<div class=\"inner picto\"><img src=\"picto/{$card['p1']}\" /></div>";
-    echo !strlen($card['p2']) ? '' : "		<div class=\"inner picto\"><img src=\"picto/{$card['p2']}\" /></div>";
-    echo !strlen($card['p3']) ? '' : "		<div class=\"inner picto\"><img src=\"picto/{$card['p3']}\" /></div>";
-    echo !strlen($card['p4']) ? '' : "		<div class=\"inner picto\"><img src=\"picto/{$card['p4']}\" /></div>";
+    echo !strlen($card['p1']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p1']) . "</div>";
+    echo !strlen($card['p2']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p2']) . "</div>";
+    echo !strlen($card['p3']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p3']) . "</div>";
+    echo !strlen($card['p4']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p4']) . "</div>";
     echo !strlen($card['p1']) ? '' : "	</div>                                           ";
     echo !strlen($card['p5']) ? '' : "	<div class=\"container right-bar\">              ";
-    echo !strlen($card['p5']) ? '' : "		<div class=\"inner picto\"><span>{$card['p5']}</span></div>           ";
-    echo !strlen($card['p5']) ? '' : "		<div class=\"inner picto\"><span>{$card['p6']}</span></div>           ";
-    echo !strlen($card['p5']) ? '' : "		<div class=\"inner picto\"><span>{$card['p7']}</span></div>           ";
+    echo !strlen($card['p5']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p5']) . "</div>           ";
+    echo !strlen($card['p5']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p6']) . "</div>           ";
+    echo !strlen($card['p5']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p7']) . "</div>           ";
     echo !strlen($card['p5']) ? '' : "	</div>                                           ";
+    // echo !strlen($card['EDS']) ? '' : "	<div class=\"container head\">{$card['EDS']}</div> ";
     echo !strlen($card['Ability box']) ? '' : "	<div class=\"container rulebox\">                ";
     echo !strlen($card['Ability box']) ? '' : "		{$card['Ability box']}                         ";
     echo !strlen($card['Ability box']) ? '' : "	</div>                                           ";
