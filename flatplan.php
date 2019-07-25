346c7dc0cdd4e9e2ed879080d280f66f03b5744e
RobertLetavernier <robert.letavernier@gmail.com>
2019-07-25 14:06:51 +0200

    add title / lower leftbar

--- flatplan.php
+++ flatplan.php
@@ -48,14 +48,15 @@ function pictify($str) {
  * Ability box
  */
  foreach ($csv as $i => $card) {
-	// if ($i < 15 OR $i > 45) continue;
+	if ($i < 15 OR $i > 45) continue;
     echo "<div class=\"cardFrame\">                            ";
 	echo '<!-- ' . print_r($card, 1) . ' -->';
     echo "<div class=\"cardBorder\">                            ";
     echo "<div class=\"cardBackground color-{$card['color']}\" style=\"background-image: url('img/{$card['id']}.jpg');\">                            ";
     echo "	<div class=\"container title\">                  ";
-    echo !strlen($card['s1']) ? '' : "	<div class=\"inner picto\">" . pictify($card['s1']) . "</div>";
-    echo !strlen($card['s2']) ? '' : "	<div class=\"inner picto\">" . pictify($card['s2']) . "</div>";
+    echo !strlen($card['s1']) ? '' : "	<div class=\"inner picto\" style=\"position:absolute; left:10px;\">" . pictify($card['s1']) . "</div>";
+    echo "	<div class=\"inner head\">" . pictify($card['Bang name']) . "</div>";
+    echo !strlen($card['s2']) ? '' : "	<div class=\"inner picto\" style=\"position:absolute; right:10px;\">" . pictify($card['s2']) . "</div>";
     echo "	</div>                                           ";
     echo !strlen($card['p1']) ? '' : "	<div class=\"container left-bar\">               ";
     echo !strlen($card['p1']) ? '' : "		<div class=\"inner picto\">" . pictify($card['p1']) . "</div>";
